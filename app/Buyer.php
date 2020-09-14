e8baa7316533b02a67d79772e7ec357c9502293e
DESKTOP-KEONA0H\LBP-PC <pglbuddhika@gmail.com>
2020-09-15 00:00:44 +0530

    model binding and Scope

--- app/Buyer.php
+++ app/Buyer.php
@@ -2,11 +2,18 @@
 
 namespace App;
 
+use App\Scopes\BuyerScope;
 use Illuminate\Database\Eloquent\Model;
 use App\Transaction;
 
 class Buyer extends User
 {
+    protected static function boot()
+    {
+        parent::boot();
+        static::addGlobalScope(new BuyerScope);
+    }
+
     public function transactions(){
         return $this->hasMany('App\Transaction');
     }
--- app/Traits/ApiResponser.php
+++ app/Traits/ApiResponser.php
@@ -22,3 +22,5 @@ trait ApiResponser{
         return $this->successResponse(['data' => $model ], $code);
     }
 }
+
+?>
